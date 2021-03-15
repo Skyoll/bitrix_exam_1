b84a6c4ffb2a33fb9bfae683d0c968d93e9f9cc1
skyoll <i.bezrukavyi@wssupport.ru>
2021-03-16 00:35:12 +0300

    ex1-15]

--- local/templates/exam1/footer.php
+++ local/templates/exam1/footer.php
@@ -1,194 +1,231 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 IncludeTemplateLangFile(__FILE__);
 ?>
 </div>
 </div>
 <!-- side -->
-                <div class="side">
-                    <!-- side menu -->
-               <?$APPLICATION->IncludeComponent(
-	"bitrix:menu", 
-	"left", 
-	array(
-		"ALLOW_MULTI_SELECT" => "N",
-		"CHILD_MENU_TYPE" => "left",
-		"DELAY" => "N",
-		"MAX_LEVEL" => "1",
-		"MENU_CACHE_GET_VARS" => array(
+<div class="side">
+    <!-- side menu -->
+	<? $APPLICATION->IncludeComponent(
+		"bitrix:menu",
+		"left",
+		array(
+			"ALLOW_MULTI_SELECT" => "N",
+			"CHILD_MENU_TYPE" => "left",
+			"DELAY" => "N",
+			"MAX_LEVEL" => "1",
+			"MENU_CACHE_GET_VARS" => array(),
+			"MENU_CACHE_TIME" => "3600",
+			"MENU_CACHE_TYPE" => "A",
+			"MENU_CACHE_USE_GROUPS" => "Y",
+			"ROOT_MENU_TYPE" => "left",
+			"USE_EXT" => "Y",
+			"COMPONENT_TEMPLATE" => "left"
 		),
-		"MENU_CACHE_TIME" => "3600",
-		"MENU_CACHE_TYPE" => "A",
-		"MENU_CACHE_USE_GROUPS" => "Y",
-		"ROOT_MENU_TYPE" => "left",
-		"USE_EXT" => "Y",
-		"COMPONENT_TEMPLATE" => "left"
-	),
-	false
-);?>
-                    <!-- /side menu -->
-                    <!-- side anonse -->
-                   <?$APPLICATION->IncludeComponent(
-    "bitrix:main.include",
-    "include_sidebar",
-    Array(
-        "AREA_FILE_RECURSIVE" => "Y",
-        "AREA_FILE_SHOW" => "sect",
-        "AREA_FILE_SUFFIX" => "inc",
-        "COMPONENT_TEMPLATE" => ".default",
-        "EDIT_TEMPLATE" => ""
-    )
-);?>
-                    <!-- /side anonse -->
-                    <!-- side wrap -->
-                    <div class="side-wrap">
-                        <div class="item-wrap">
-                            <!-- side action -->
-                            <div class="side-block side-action">
-                                <img src="<?=SITE_TEMPLATE_PATH?>/img/side-action-bg.jpg" alt="" class="bg">
-                                <div class="photo-block">
-                                    <img src="<?=SITE_TEMPLATE_PATH?>/img/side-action.jpg" alt="">
-                                </div>
-                                <div class="text-block">
-                                    <div class="title">Акция!</div>
-                                    <p>Мебельная полка всего за 560 <span class="r">a</span>
-                                    </p>
-                                </div>
-                                <a href="" class="btn-more">подробнее</a>
-                            </div>
-                            <!-- /side action -->
-                        </div>
-                        
-                       <!-- footer rew slider box -->
-                       <div class="item-wrap">
-		                     <div class="rew-footer-carousel">
-								<div class="item">
-									<div class="side-block side-opin">
-										<div class="inner-block">
-											<div class="title">
-												<div class="photo-block">
-													<img src="<?=SITE_TEMPLATE_PATH?>/img/side-opin.jpg" alt="">
-												</div>
-												<div class="name-block"><a href="">Дмитрий Иванов</a></div>
-												<div class="pos-block">Генеральный директор,"Офис+"</div>
-											</div>
-											<div class="text-block">“В магзине предоставили потрясающий выбор
-												расцветок, а также, получил большую скидку по карте постоянного...</div>
-										</div>
-									</div>
-								</div>
-								<div class="item">
-									<div class="side-block side-opin">
-										<div class="inner-block">
-											<div class="title">
-												<div class="photo-block">
-													<img src="<?=SITE_TEMPLATE_PATH?>/img/side-opin.jpg" alt="">
-												</div>
-												<div class="name-block"><a href="">Дмитрий Иванов</a></div>
-												<div class="pos-block">Генеральный директор,"Офис+"</div>
-											</div>
-											<div class="text-block">“В магазине предоставили потрясающий выбор
-												расцветок, а также, получил большую скидку по карте постоянного...</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<!-- / footer rew slider box --> 
-                        
-                    </div>
-                    <!-- /side wrap -->
+		false
+	); ?>
+    <!-- /side menu -->
+    <!-- side anonse -->
+	<? $APPLICATION->IncludeComponent(
+		"bitrix:main.include",
+		"include_sidebar",
+		array(
+			"AREA_FILE_RECURSIVE" => "Y",
+			"AREA_FILE_SHOW" => "sect",
+			"AREA_FILE_SUFFIX" => "inc",
+			"COMPONENT_TEMPLATE" => ".default",
+			"EDIT_TEMPLATE" => ""
+		)
+	); ?>
+    <!-- /side anonse -->
+    <!-- side wrap -->
+    <div class="side-wrap">
+        <div class="item-wrap">
+            <!-- side action -->
+            <div class="side-block side-action">
+                <img src="<?= SITE_TEMPLATE_PATH ?>/img/side-action-bg.jpg" alt="" class="bg">
+                <div class="photo-block">
+                    <img src="<?= SITE_TEMPLATE_PATH ?>/img/side-action.jpg" alt="">
                 </div>
-                <!-- /side -->
+                <div class="text-block">
+                    <div class="title">Акция!</div>
+                    <p>Мебельная полка всего за 560 <span class="r">a</span>
+                    </p>
+                </div>
+                <a href="" class="btn-more">подробнее</a>
             </div>
-            <!-- /content box -->
+            <!-- /side action -->
         </div>
-        <!-- /page -->
-        <div class="empty"></div>
-    </div>
-    <!-- /wrap -->
-    <!-- footer -->
-    <footer class="footer">
-        <div class="inner-wrap">
-            <nav class="main-menu">
-                <?$APPLICATION->IncludeComponent(
-    "bitrix:menu",
-    "bottom",
-    Array(
-        "ALLOW_MULTI_SELECT" => "N",
-        "CHILD_MENU_TYPE" => "left",
-        "COMPONENT_TEMPLATE" => ".default",
-        "DELAY" => "N",
-        "MAX_LEVEL" => "1",
-        "MENU_CACHE_GET_VARS" => "",
-        "MENU_CACHE_TIME" => "3600",
-        "MENU_CACHE_TYPE" => "N",
-        "MENU_CACHE_USE_GROUPS" => "Y",
-        "ROOT_MENU_TYPE" => "bottom",
-        "USE_EXT" => "N"
-    )
-);?>
-                <div class="item">
-                    <div class="title-block">Каталог товаров</div>
-                    <ul>
-                        <li><a href="">Кухни</a>
-                        </li>
-                        <li><a href="">Гарнитуры</a>
-                        </li>
-                        <li><a href="">Спальни и матрасы</a>
-                        </li>
-                        <li><a href="">Столы и стулья</a>
-                        </li>
-                        <li><a href="">Раскладные диваны</a>
-                        </li>
-                        <li><a href="">Кресла</a>
-                        </li>
-                        <li><a href="">Кровати и кушетки</a>
-                        </li>
-                        <li><a href="">Тумобчки и прихожие</a>
-                        </li>
-                        <li><a href="">Аксессуары</a>
-                        </li>
-                        <li><a href="">Каталоги мебели</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-            <div class="contacts-block">
-                <div class="title-block"><?=GetMessage("CONTACT_IFORMATION")?></div>
-                <div class="loc-block">
-                    <div class="address">ул. Летняя, стр.12, офис 512</div>
-                    <?$APPLICATION->IncludeComponent(
-	"bitrix:main.include", 
-	".default", 
+
+        <!-- footer rew slider box -->
+        <div class="item-wrap">
+			
+			
+			<? $APPLICATION->IncludeComponent(
+	"bitrix:news.list", 
+	"side_bar_rew", 
 	array(
-		"COMPONENT_TEMPLATE" => ".default",
-		"AREA_FILE_SHOW" => "file",
-		"PATH" => "/include/phone.php",
-		"EDIT_TEMPLATE" => ""
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"ADD_SECTIONS_CHAIN" => "N",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "N",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"CHECK_DATES" => "Y",
+		"DETAIL_URL" => "#SITE_DIR#/rew/#ELEMENT_CODE#/",
+		"DISPLAY_BOTTOM_PAGER" => "N",
+		"DISPLAY_DATE" => "N",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"FIELD_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"FILTER_NAME" => "",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"IBLOCK_ID" => "5",
+		"IBLOCK_TYPE" => "news",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+		"INCLUDE_SUBSECTIONS" => "N",
+		"MESSAGE_404" => "",
+		"NEWS_COUNT" => "2",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "Новости",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"PROPERTY_CODE" => array(
+			0 => "POSITION",
+			1 => "COMPANY",
+			2 => "",
+		),
+		"SET_BROWSER_TITLE" => "N",
+		"SET_LAST_MODIFIED" => "N",
+		"SET_META_DESCRIPTION" => "N",
+		"SET_META_KEYWORDS" => "N",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "N",
+		"SHOW_404" => "N",
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_BY2" => "NAME",
+		"SORT_ORDER1" => "DESC",
+		"SORT_ORDER2" => "ASC",
+		"STRICT_SECTION_CHECK" => "N",
+		"COMPONENT_TEMPLATE" => "side_bar_rew"
 	),
 	false
-);?><br>
-                
-                </div>
-                <div class="main-soc-block">
-                    <a href="" class="soc-item">
-                        <img src="<?=SITE_TEMPLATE_PATH?>/img/icons/soc01.png" alt="">
-                    </a>
-                    <a href="" class="soc-item">
-                        <img src="<?=SITE_TEMPLATE_PATH?>/img/icons/soc02.png" alt="">
-                    </a>
-                    <a href="" class="soc-item">
-                        <img src="<?=SITE_TEMPLATE_PATH?>/img/icons/soc03.png" alt="">
-                    </a>
-                    <a href="" class="soc-item">
-                        <img src="<?=SITE_TEMPLATE_PATH?>/img/icons/soc04.png" alt="">
-                    </a>
-                </div>
-                <div class="copy-block">© 2000 - 2012 "Мебельный магазин"</div>
-            </div>
+); ?>
+
+
         </div>
-    </footer>
-    <!-- /footer -->
+        <!-- / footer rew slider box -->
+
+    </div>
+    <!-- /side wrap -->
+</div>
+<!-- /side -->
+</div>
+<!-- /content box -->
+</div>
+<!-- /page -->
+<div class="empty"></div>
+</div>
+<!-- /wrap -->
+<!-- footer -->
+<footer class="footer">
+    <div class="inner-wrap">
+        <nav class="main-menu">
+			<? $APPLICATION->IncludeComponent(
+				"bitrix:menu",
+				"bottom",
+				array(
+					"ALLOW_MULTI_SELECT" => "N",
+					"CHILD_MENU_TYPE" => "left",
+					"COMPONENT_TEMPLATE" => ".default",
+					"DELAY" => "N",
+					"MAX_LEVEL" => "1",
+					"MENU_CACHE_GET_VARS" => "",
+					"MENU_CACHE_TIME" => "3600",
+					"MENU_CACHE_TYPE" => "N",
+					"MENU_CACHE_USE_GROUPS" => "Y",
+					"ROOT_MENU_TYPE" => "bottom",
+					"USE_EXT" => "N"
+				)
+			); ?>
+            <div class="item">
+                <div class="title-block">Каталог товаров</div>
+                <ul>
+                    <li><a href="">Кухни</a>
+                    </li>
+                    <li><a href="">Гарнитуры</a>
+                    </li>
+                    <li><a href="">Спальни и матрасы</a>
+                    </li>
+                    <li><a href="">Столы и стулья</a>
+                    </li>
+                    <li><a href="">Раскладные диваны</a>
+                    </li>
+                    <li><a href="">Кресла</a>
+                    </li>
+                    <li><a href="">Кровати и кушетки</a>
+                    </li>
+                    <li><a href="">Тумобчки и прихожие</a>
+                    </li>
+                    <li><a href="">Аксессуары</a>
+                    </li>
+                    <li><a href="">Каталоги мебели</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div class="contacts-block">
+            <div class="title-block"><?= GetMessage("CONTACT_IFORMATION") ?></div>
+            <div class="loc-block">
+                <div class="address">ул. Летняя, стр.12, офис 512</div>
+				<? $APPLICATION->IncludeComponent(
+					"bitrix:main.include",
+					".default",
+					array(
+						"COMPONENT_TEMPLATE" => ".default",
+						"AREA_FILE_SHOW" => "file",
+						"PATH" => "/include/phone.php",
+						"EDIT_TEMPLATE" => ""
+					),
+					false
+				); ?><br>
+
+            </div>
+            <div class="main-soc-block">
+                <a href="" class="soc-item">
+                    <img src="<?= SITE_TEMPLATE_PATH ?>/img/icons/soc01.png" alt="">
+                </a>
+                <a href="" class="soc-item">
+                    <img src="<?= SITE_TEMPLATE_PATH ?>/img/icons/soc02.png" alt="">
+                </a>
+                <a href="" class="soc-item">
+                    <img src="<?= SITE_TEMPLATE_PATH ?>/img/icons/soc03.png" alt="">
+                </a>
+                <a href="" class="soc-item">
+                    <img src="<?= SITE_TEMPLATE_PATH ?>/img/icons/soc04.png" alt="">
+                </a>
+            </div>
+            <div class="copy-block">© 2000 - 2012 "Мебельный магазин"</div>
+        </div>
+    </div>
+</footer>
+<!-- /footer -->
 </body>
 
 </html>
