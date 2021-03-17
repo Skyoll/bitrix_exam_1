b12765aeb545ea8bc438ac37939a5d32918f2fe8
skyoll <i.bezrukavyi@wssupport.ru>
2021-03-18 00:54:16 +0300

    [ex1-20]

---
+++ login/user.php
@@ -0,0 +1,23 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("профиль пользователя");
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:main.profile",
+	"",
+	Array(
+		"CHECK_RIGHTS" => "N",
+		"SEND_INFO" => "N",
+		"SET_TITLE" => "Y",
+		"USER_PROPERTY" => array(),
+		"USER_PROPERTY_NAME" => ""
+	)
+);?><?$APPLICATION->IncludeComponent(
+	"bitrix:system.auth.form",
+	"",
+	Array(
+		"FORGOT_PASSWORD_URL" => "/login/?forgot_password=yes",
+		"PROFILE_URL" => "/login/user.php",
+		"REGISTER_URL" => "/login/?register=yes",
+		"SHOW_ERRORS" => "N"
+	)
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
